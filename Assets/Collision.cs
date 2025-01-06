af7b17132775bc1e7bffdaecf621e7cc1d97cc97
hiTech <braykul2605@gmail.com>
2025-01-06 19:26:09 +0700

    Add collision detection and new game objects to the scene

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,4 +5,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("What was that?!");
+    }
 }
