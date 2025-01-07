bbf742bdf94a0d9cb887a66fe9b6fe42bc6add38
hiTech <braykul2605@gmail.com>
2025-01-07 08:23:13 +0700

    Revert "Add collision detection and new game objects to the scene"
    This reverts commit af7b17132775bc1e7bffdaecf621e7cc1d97cc97.

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,8 +5,4 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
-
-    void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("What was that?!");
-    }
 }
